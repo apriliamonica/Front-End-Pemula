fd062d8851d821e11b7ff59650c15ba62a41b0df
April <liapandeiroth34190@gmail.com>
2024-09-06 15:51:47 +0800

    event onFocus

--- input event/index.js
+++ input event/index.js
@@ -1,4 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
   const inputMaxLengthOnLoad = document.getElementById("inputNama").maxLength;
   document.getElementById("sisaKarakter").innerText = inputMaxLengthOnLoad;
+
+  document.getElementById("inputNama").addEventListener("input", function () {
+    /* Kode lainnya disembunyikan */
+  });
+
+  document.getElementById("inputNama").addEventListener("focus", function () {
+    console.log("inputNama: focus");
+    document.getElementById("notifikasiSisaKarakter").style.visibility =
+      "visible";
+  });
 });
